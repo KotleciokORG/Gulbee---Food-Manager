dc3420541d31cfd7cbd2881b9d8eff8c1b855e61
KacperJodjod <kacperosj2003@gmail.com>
2024-09-20 22:05:13 +0200

    Trying to add communication from Form
    Still a lot of Errors

--- Gulbee.Api/obj/Debug/net8.0/Gulbee.Api.AssemblyInfo.cs
+++ Gulbee.Api/obj/Debug/net8.0/Gulbee.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gulbee.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bb67cf257d32e5ffc4fef61bbaad6b7561c8365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c05afac4aa4ffc36d793e569d6bb2952004d7287")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gulbee.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gulbee.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
